17b66956d331922ebb79bc93a99e07ef93a08c39
sl@minta.com <sl@minta.com>
2012-06-04 21:47:45 -0500

    submit payments

--- PoS/payment-processor/controller.py
+++ PoS/payment-processor/controller.py
@@ -17,7 +17,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 #
-import sys, pcos
+import sys, pcos, urllib2, binascii, time
 from decimal import Decimal
 from PySide.QtCore import *
 import settings as const
@@ -34,45 +34,45 @@ class AppController(QObject):
 		'''Handles PTA'''
 
 		# public block of PTA
-		p1 = self.doc.block( 'P1' )
+		p1 = self.payload.doc.block( 'P1' )
 		
-		self.pta = Segment('PTA')
+		self.payload.pta = Segment('PTA')
 
 		# parse PTA's public members
-		self.pta.ctime = p1.read_int64() # create time
-		self.pta.expiry = p1.read_int64() # expiry
+		self.payload.pta.ctime = p1.read_int64() # create time
+		self.payload.pta.expiry = p1.read_int64() # expiry
 		
 		# payment limit
-		self.pta.payment_limit = decimal_from_parts(p1.read_int64(), p1.read_int16())
+		self.payload.pta.payment_limit = decimal_from_parts(p1.read_int64(), p1.read_int16())
 		
 		# optional gratuity
-		self.pta.has_tip = bool( p1.read_byte() ) # has tip?
-		self.pta.tip = Decimal("0.00")
-		if self.pta.has_tip:
+		self.payload.pta.has_tip = bool( p1.read_byte() ) # has tip?
+		self.payload.pta.tip = Decimal("0.00")
+		if self.payload.pta.has_tip:
 			# gratuity type
-			self.pta.tip_type = p1.read_fixed_string(1)
-			if self.pta.tip_type not in ('A', 'P'):
-				raise pcos.PcosError( const.ERR_INVALID_GRATUITY_TYPE, "PTA: '%s' is not a supported gratuity type" % self.pta.tip_type)
+			self.payload.pta.tip_type = p1.read_fixed_string(1)
+			if self.payload.pta.tip_type not in ('A', 'P'):
+				raise pcos.PcosError( const.ERR_INVALID_GRATUITY_TYPE, "PTA: '%s' is not a supported gratuity type" % self.payload.pta.tip_type)
 			
 			# gratuity amount
-			self.pta.tip = decimal_from_parts(p1.read_int64(), p1.read_int16())
-			if self.pta.tip < 0:
+			self.payload.pta.tip = decimal_from_parts(p1.read_int64(), p1.read_int16())
+			if self.payload.pta.tip < 0:
 				raise pcos.PcosError( const.ERR_VALUE_OUT_OF_RANGE, "PTA: gratuity cannot be negative")
 
 		# currency
-		self.pta.currency = p1.read_fixed_string(3).upper()
+		self.payload.pta.currency = p1.read_fixed_string(3).upper()
 		# presently we only deal with US dollars
-		if self.pta.currency != 'USD':
-			raise pcos.PcosError( const.ERR_INVALID_CURRENCY, "PTA: '%s' is not a supported currency" % self.pta.currency)
+		if self.payload.pta.currency != 'USD':
+			raise pcos.PcosError( const.ERR_INVALID_CURRENCY, "PTA: '%s' is not a supported currency" % self.payload.pta.currency)
 
 		# private key identifier, not used here
 		keyid = p1.read_fixed_string(4)
 
-		self.pta.receiver = p1.read_short_string() # email of the payment receiver
-		self.pta.note = p1.read_short_string() # note
+		self.payload.pta.receiver = p1.read_short_string() # email of the payment receiver
+		self.payload.pta.note = p1.read_short_string() # note
 
 		# return PTA object (acutally, just the public segment of it)
-		return self.pta
+		return self.payload.pta
 
 
 	#-------------------------------
@@ -81,6 +81,10 @@ class AppController(QObject):
 	def __init__(self):
 		QObject.__init__(self)
 
+		# empty payload
+		self.payload = None
+
+		# payload handlers
 		self.lookup = {
 			'Pa': self.on_payment_authorization,
 		}
@@ -88,15 +92,17 @@ class AppController(QObject):
 
 	def parse_pcos(self, data):
 		try:
-			self.doc = pcos.Doc( data )
+			self.payload = Segment('Body')
+			self.payload.data = data
+			self.payload.doc = pcos.Doc( data )
 			# find the message handler
-			handler = self.lookup.get( self.doc.message_id, None )
+			handler = self.lookup.get( self.payload.doc.message_id, None )
 			if handler:
 				# handler found, process the message and emit results
 				self.onDataArrived.emit( handler() )
 
 			else: # unknown request
-				raise RuntimeError("Unsupported PTA message: %s" % self.doc.message_id)
+				raise RuntimeError("Unsupported PTA message: %s" % payload.doc.message_id)
 
 		except Exception, e:
 			err = Segment('Er')
@@ -104,9 +110,118 @@ class AppController(QObject):
 			self.onDataArrived(err)
 
 
+	def submit(self, form):
+		'''Submits PTA to the server'''
+		try:
+			if not (self.payload and hasattr(self.payload, 'pta')):
+				raise RuntimeError("No Payment Certificate")
+
+			# package PTA into a block
+			pta = pcos.Block( 'Pa', 512, 'O' )
+			pta.write_fixed_string(self.payload.data, size=len(self.payload.data))
+
+			# create payment-request block
+			r1 = pcos.Block( 'R1', 1024, 'O' )
+			r1.write_fixed_string( binascii.unhexlify( const.MERCHANT_MAT ), size=20 ) # mat
+			r1.write_short_string( '', max=127 ) # ref_data
+			r1.write_int64( long( time.time() + 0.5 ) ) # request create-time
+
+			# charge amount
+			(charge_value, charge_scale) = decimal_to_parts(form.charge)
+			r1.write_int64( charge_value ) # value
+			r1.write_int16( charge_scale ) # scale
+
+			# tax: basic terminal app doesn't expect user to enter this
+			r1.write_byte(0) # no tax info
+
+			# tip
+			if form.tip:
+				r1.write_byte(1) # has tip info
+				(tip_value, tip_scale) = decimal_to_parts(form.tip)
+				r1.write_int64( tip_value ) # value
+				r1.write_int16( tip_scale ) # scale
+			else:
+				r1.write_byte(0) # no tip info
+
+			r1.write_fixed_string( const.CURRENCY_CODE, size=3 ) # currency
+			r1.write_short_string( form.order_id, max=24 ) # invoice ID
+			r1.write_short_string( form.note, max=127 ) # note
+			r1.write_int16(0) # list of purchased goods
+
+			# package everything and ship out
+			req = pcos.Doc( name="Pt" )
+			req.add( pta )
+			req.add( r1 )
+
+			res = self.send( req )
+			self.expect_success( res, form.charge, form.order_id, form.note )
+
+		except Exception, e:
+#TODO			log.error('Success (tx_id: %s, ref_data: %s)', binascii.hexlify( transaction_id ), binascii.hexlify( ref_data ))
+			err = Segment('Er')
+			err.what = str(e)
+
+			# emit the message
+			self.onDataArrived.emit(err)
+
+
 	def reset(self):
-		self.pta = None
+		self.payload = None
 		self.onDataArrived.emit( Segment('Clear') )
+
+
+	def expect_success( self, res, charge, order_id, note ):
+		'''Shows details of Success PCOS message'''
+		if res.message_id == "Ok":
+			bo = res.block( 'Bo' )
+			ref_data = bo.read_short_string() # ref_data
+			transaction_id = bo.read_short_string() # tx-id
+
+			ok = Segment('Ok')
+			time_prt = time.strftime("%x %X", time.localtime(time.time()))
+			ok.what = '<p align="right">%s</p><center><h1><font color="#90ff90"><strong>Approved</strong></font></h1></center><center><h2>$%s</h2></center>' % (time_prt, charge)
+			ok.what += '<br /><font size="2">ID: %s</font>' % binascii.hexlify( transaction_id )
+			if order_id:
+				ok.what += '<br /><font size="2">Order: %s</font>' % order_id
+			if note:
+				ok.what += '<br /><font size="2">Note: %s</font>' % note
+
+			# after submission and successful return, reset state
+			self.payload = None
+
+			# emit the message
+			self.onDataArrived.emit(ok)
+
+#TODO			log.info('Success (tx_id: %s, ref_data: %s)', binascii.hexlify( transaction_id ), binascii.hexlify( ref_data ))
+		else:
+			raise RuntimeError("'%s' not a Success message" % res.message_id)
+
+
+	def send(self, req):
+		'''Sends request to the server, returns result'''
+		# Get encoded PCOS data 	
+		encoded = req.encoded()
+
+		remote_call = urllib2.urlopen(const.PUSHCOIN_SERVER_URL, encoded )
+		response = remote_call.read()
+
+		res = pcos.Doc( response )
+		# check if response is not an error
+		if res.message_id == 'Er':
+			# jump to the block of interest
+			er = res.block( 'Bo' )
+			if er:
+				ref_data = er.read_short_string();
+				transaction_id = er.read_short_string();
+				code = er.read_int32();
+				what = er.read_short_string();
+				raise RuntimeError('%s\nCode # %s\nTransaction ID:\n %s' % (what, code, binascii.hexlify( transaction_id ))) 
+			else:
+				raise RuntimeError('ERROR -- cause unknown') 
+
+		# return a lightweight PCOS document 
+		return res
+
 
 
 def decimal_to_parts(value):
